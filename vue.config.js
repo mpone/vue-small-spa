ccac752c70a819fb05b40d02337e08527de86780
Mykhailo Parfeniuk <mypa@brighta.biz>
2023-04-16 00:49:03 +0300

    pre build

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: [
-    'vuetify'
-  ]
+    publicPath: "/vue-small-spa/",
+    transpileDependencies: [
+        'vuetify'
+    ],
 })
