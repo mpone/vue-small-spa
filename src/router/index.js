39843037f4b903ec6e96d54f8973d1abfbdaf4b5
Mykhailo Parfeniuk <mypa@brighta.biz>
2023-04-15 22:02:02 +0300

    project in progress (without Vuex)

---
+++ src/router/index.js
@@ -0,0 +1,23 @@
+import Vue from "vue"
+import VueRouter from "vue-router"
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: "/",
+    component: () => import("/src/pages/MainPage.vue")
+  },
+  {
+    path: "/photo",
+    component: () => import("/src/pages/PhotoPage.vue")
+  }
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes
+})
+
+export default router
